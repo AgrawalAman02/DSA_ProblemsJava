062fb9290de079ade1d6a94812fc4db89ac6af09
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-06 23:50:17 +0530

    Updated BubbleSort.java

--- GeeksForGeeks/Sorting/BubbleSort.java
+++ GeeksForGeeks/Sorting/BubbleSort.java
@@ -5,13 +5,16 @@ class Solution
     {
         //code here
         for(int  i = 0 ; i <n; i++){
+	    int didSwap = 0;         // added a tracker to keep track of the best case .
             for (int j =  0 ; j<n-i-1; j++){
                 if(arr[j+1]<arr[j]) {
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
+	            didSwap = 1;
                 }
             }
+		if(didSwap==0) break;   // if swap hadnt taken place in any turn then it means that the arr is sorted , so break;
         }
         
     }
