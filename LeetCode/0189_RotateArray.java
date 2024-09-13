f992765d84ea17ac96e1e43cb0c665ad4255a31c
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-14 00:38:07 +0530

    Create 0189_RotateArray.java---BruteForceApproach

---
+++ LeetCode/0189_RotateArray.java
@@ -0,0 +1,34 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+
+
+// brute force approach -- yeah taking an extra space 
+
+
+class Solution {
+    public void rotate(int[] arr, int k) {
+        
+        int n = arr.length;
+        k = k%n;   // if no. of rotation is more than the length of the array so we are finding the modulo of it
+        if(arr.length == 1 || k== 0) return;   // if arr size is of length 1 or no. of rotation is of length == arrSize then it is the  same array , so return
+        int temp[] = new int[n-k];    // storing the elements that are going to rotate in temp array thats why initialise that array 
+        
+        for(int i =0 ; i<n-k; i++){
+            temp [i] = arr[i];
+        }
+        for(int i =0 ; i<k; i++){
+            arr[i] = arr[n-k+i];
+        }
+        for(int i = 0; i<n-k; i++){
+            arr[i+k] = temp[i];
+        }
+    }
+}
