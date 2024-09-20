31e5a4d14e91255e70056c8144881936f52075c6
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-20 21:58:42 +0530

    Update 0169 with O(1) space complexity

--- LeetCode/0169_MajorityElement.java
+++ LeetCode/0169_MajorityElement.java
@@ -1,18 +1,30 @@
 // my first approach
 
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util*;
 class Solution {
     public int majorityElement(int[] arr) {
         int n = arr.length;
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i = 0 ; i<n;i++){
-            mp.put(arr[i], mp.getOrDefault(arr[i],0) +1);
-        }
+        // HashMap<Integer,Integer> mp = new HashMap<>();
+        // for(int i = 0 ; i<n;i++){
+        //     mp.put(arr[i], mp.getOrDefault(arr[i],0) +1);
+        // }
         
-        for(Entry<Integer,Integer> entry : mp.entrySet()){
-            if(entry.getValue()>n/2) return entry.getKey(); 
+        // for(Entry<Integer,Integer> entry : mp.entrySet()){
+        //     if(entry.getValue()>n/2) return entry.getKey(); 
+        // }
+        // return -1;
+
+        // with O(1) space 
+        int element = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i = 0; i<n;i++){
+            if(count ==0){
+                element = arr[i];
+                count ++;
+            }
+            else if(arr[i]== element) count++;
+            else if(arr[i]!= element) count--;
         }
-        return -1;
+        return element;
     }
 }
