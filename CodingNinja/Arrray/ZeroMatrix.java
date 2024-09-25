03c2620dc249462249f548148a9517ebb13de71c
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-25 22:37:48 +0530

    Create ZeroMatrix.java

---
+++ CodingNinja/Arrray/ZeroMatrix.java
@@ -0,0 +1,29 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+    	// Write your code here.
+        int [] row = new int[n];
+        int []col = new int[m];
+
+        for(int i = 0; i<n;i++){
+            for(int j  = 0; j<m; j++){
+                if(matrix.get(i).get(j)==0){
+                    row[i]=1;
+                    col[j] = 1;
+
+                }
+            }
+        }
+
+        for(int i = 0; i<n;i++){
+            for(int j  = 0; j<m; j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix.get(i).set(j, 0);
+
+                }
+            }
+        }
+        return matrix;
+    }
+}
