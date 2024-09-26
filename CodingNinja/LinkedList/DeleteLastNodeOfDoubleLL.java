55dd5fd555fa43ad37a45e6b02aaf8154930c962
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-27 01:02:59 +0530

    Create DeleteLastNodeOfDoubleLL.java

---
+++ CodingNinja/LinkedList/DeleteLastNodeOfDoubleLL.java
@@ -0,0 +1,50 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node deleteLastNode(Node head) {
+        // Write your code here
+        Node temp = head;
+        while(temp.next != null && temp.next.next != null){
+            temp = temp.next;
+        }
+        if(temp.next == null) return null;
+        else{
+            temp.next = null;
+            return head;
+        
+        }
+        
+    }
+}
