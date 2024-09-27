8cddada771d8d6172725a0f9bdb5e605bbf0329c
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-27 20:10:52 +0530

    Create MiddleOfLL.java

---
+++ CodingNinja/LinkedList/MiddleOfLL.java
@@ -0,0 +1,42 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node findMiddle(Node head)
+    {
+        // Write your code here.
+        Node slow = head;
+        Node fast = head;
+        while(fast != null){
+            if(fast.next == null) return slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
