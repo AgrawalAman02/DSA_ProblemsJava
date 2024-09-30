88d216d9198c6bd914b943f490a77e548251c867
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-30 13:31:06 +0530

    Optimise SortLL0s1s2s.java - -O(N) time complexity

--- GeeksForGeeks/LinkedList/SingleLL/SortLL0s1s2s.java
+++ GeeksForGeeks/LinkedList/SingleLL/SortLL0s1s2s.java
@@ -33,58 +33,94 @@ class Node
 class Solution {
     // Function to sort a linked list of 0s, 1s and 2s.
     
-    static Node merge(Node left, Node right) {
-        Node dummy = new Node(-1);
-        Node temp = dummy;
+    // static Node merge(Node left, Node right) {
+    //     Node dummy = new Node(-1);
+    //     Node temp = dummy;
 
-        while (left != null && right != null) {
-            if (left.data < right.data) {
-                temp.next = left;
-                left = left.next;
-            } else {
-                temp.next = right;
-                right = right.next;
-            }
-            temp = temp.next;
-        }
+    //     while (left != null && right != null) {
+    //         if (left.data < right.data) {
+    //             temp.next = left;
+    //             left = left.next;
+    //         } else {
+    //             temp.next = right;
+    //             right = right.next;
+    //         }
+    //         temp = temp.next;
+    //     }
 
-        if (left != null) {
-            temp.next = left;
-        } else {
-            temp.next = right;
-        }
+    //     if (left != null) {
+    //         temp.next = left;
+    //     } else {
+    //         temp.next = right;
+    //     }
 
-        return dummy.next;
-    }
+    //     return dummy.next;
+    // }
 
-    static Node middlell(Node head){
-        if(head == null || head.next == null) return head;
+    // static Node middlell(Node head){
+    //     if(head == null || head.next == null) return head;
 
-        Node fast = head.next;
-        Node slow = head;
+    //     Node fast = head.next;
+    //     Node slow = head;
         
-        while(fast!= null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
+    //     while(fast!= null && fast.next != null){
+    //         fast = fast.next.next;
+    //         slow = slow.next;
 
-        }
-        return slow;
-    }
+    //     }
+    //     return slow;
+    // }
 
     
     static Node segregate(Node head) {
         // add your code here
-        if(head ==null || head.next == null) return head;
+        // if(head ==null || head.next == null) return head;
 
-        Node middle = middlell(head);
-        Node left = head;
-        Node right = middle.next;
-        middle.next = null;
+        // Node middle = middlell(head);
+        // Node left = head;
+        // Node right = middle.next;
+        // middle.next = null;
 
-        left = segregate(left);
-        right = segregate(right);
+        // left = segregate(left);
+        // right = segregate(right);
 
-        return merge(left,right);
+        // return merge(left,right);
+
+        // we had used time comp of O(Nlog(N)) above now we will use the three pointer to reduce t.c
+        
+        
+        Node zeroD  = new Node(-1);
+        Node oneD  = new Node(-1);
+        Node twoD  = new Node(-1);
+        
+        Node zero = zeroD , one = oneD , two = twoD;
+        
+        Node temp = head;
+        while(temp!= null){
+            if(temp.data == 0){
+                zero.next = temp;
+                zero  = zero.next;
+            }else if(temp.data == 1){
+                one.next = temp;
+                one = one.next;
+                
+            }
+            else{
+                two.next = temp;
+                two = two.next;
+                
+            }
+            temp = temp.next;
+        }
+        
+        zero.next = (oneD.next!= null) ? oneD.next : twoD.next;
+        one.next = twoD.next;
+        two.next = null;
+        
+        head = oneD.next;
+        return head;
+        
+        // this will use the time complexity O(N); space complexity obv :O(1);
     }
 }
 
