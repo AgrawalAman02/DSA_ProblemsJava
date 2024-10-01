026cfe1ebe03bfbed74277f9c07165dcce62f774
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-10-01 21:55:22 +0530

    Create RemoveDuplicatesFromSortedDLL.java

---
+++ GeeksForGeeks/LinkedList/DoublyLL/RemoveDuplicatesFromSortedDLL.java
@@ -0,0 +1,25 @@
+class Solution{
+    Node removeDuplicates(Node head){
+        // Code Here.
+        
+        Node curr= head;
+        int track= curr.data;
+        curr = curr.next;
+        while(curr!= null){
+            if(curr.data == track){
+                if(curr.next == null){
+                    curr.prev.next = null;
+                }
+                else
+                {
+                    curr.prev.next = curr.next;
+                    curr.next.prev = curr.prev;
+                }
+            }else{
+                track = curr.data;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
