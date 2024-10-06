6b74f161d6687ee268207404ff1fb94b1c20dc4b
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-10-06 15:27:45 +0530

    Create 0015_ThreeSum.java

---
+++ LeetCode/0015_ThreeSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n = arr.length;
+        Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                //Calculate the 3rd element:
+                int third = -(arr[i] + arr[j]);
+
+                //Find the element in the set:
+                if (hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+
+        // store the set elements in the answer:
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+}
