bad6829cfe2063e77352b11ef9a8dfc97c13d3d5
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-10-21 21:50:19 +0530

    Create PaintersProblem.java

---
+++ CodingNinja/BinarySearch/PaintersProblem.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Solution 
+{
+    public static int countPainters(ArrayList<Integer> boards, int time) {
+        int n = boards.size(); // size of array.
+        int painters = 1;
+        long boardsPainter = 0;
+        for (int i = 0; i < n; i++) {
+            if (boardsPainter + boards.get(i) <= time) {
+                // allocate board to current painter
+                boardsPainter += boards.get(i);
+            } else {
+                // allocate board to next painter
+                painters++;
+                boardsPainter = boards.get(i);
+            }
+        }
+        return painters;
+    }
+
+    public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
+        int low  = Integer.MIN_VALUE;
+        int high = 0;
+        for(int i = 0; i<boards.size(); i++){
+            low = Math.max(low,boards.get(i));
+            high+=boards.get(i);
+        }
+        // Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int painters = countPainters(boards, mid);
+            if (painters > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+}
