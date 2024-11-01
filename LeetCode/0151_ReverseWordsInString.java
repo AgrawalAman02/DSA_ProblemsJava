819d64c37c8d8268029b9672838f47263fe13830
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-11-01 23:30:03 +0530

    Update 0151_ReverseWordsInString.java

--- LeetCode/0151_ReverseWordsInString.java
+++ LeetCode/0151_ReverseWordsInString.java
@@ -25,3 +25,36 @@ class Solution {
         return str.toString();
     }
 }
+// my other approach 
+
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        String temp = "";
+        StringBuilder str = new StringBuilder();
+        boolean firstWord = true;  // Track the first word to handle spaces
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                temp += ch;
+            } else if (!temp.isEmpty()) { // Only add to str if temp has a word
+                if (firstWord) {
+                    str.insert(0, temp);
+                    firstWord = false;
+                } else {
+                    str.insert(0, temp+" ");
+                }
+                temp = "";
+            }
+        }
+
+        // Add the last word if it exists
+        if (!temp.isEmpty()) {
+            if (!firstWord) str.insert(0, " ");
+            str.insert(0, temp);
+        }
+
+        return str.toString();
+    }
+}
