48d2f011f4466e6691020ea8ab7fddfe5910766a
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-11-01 23:24:26 +0530

    Create 0151_ReverseWordsInString.java

---
+++ LeetCode/0151_ReverseWordsInString.java
@@ -0,0 +1,27 @@
+// my first approach 
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        String temp = "";
+        StringBuilder str = new StringBuilder("");
+        int cnt =0;
+        for(int i = 0 ;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch != ' '){
+                temp += ch;
+                cnt = 0;
+
+            }else{
+                cnt++;
+                if(cnt==1)  str.insert(0," "+temp);
+                temp = "";
+            }
+        }
+        str.insert(0,temp);
+        if(str.charAt(0) == ' ') str.delete(0,1);
+        n = str.length();
+        if(str.charAt(n-1)==' ') str.delete(n-1,n);
+
+        return str.toString();
+    }
+}
